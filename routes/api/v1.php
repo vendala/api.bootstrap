4ee20376d5f8fcd15294f408d6cde39260f58738
SOSTheBlack <jeancesargarcia@gmail.com>
2019-03-20 00:22:33 -0300

    psr

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -8,6 +8,6 @@ $api;
 
 $api->get('/ping-pong', ['as' => 'ping-pong', 'uses' => 'PingPongController@__invoke']);
 
-$api->group(['namespace' => 'Users', 'prefix' => '/users'], function (\Dingo\Api\Routing\Router              $api) {
+$api->group(['namespace' => 'User', 'prefix' => '/users'], function (\Dingo\Api\Routing\Router $api) {
     $api->post('/', ['as' => 'users.storage', 'uses' => 'StorageController@__invoke']);
 });
