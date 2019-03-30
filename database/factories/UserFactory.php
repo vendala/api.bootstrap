a75f38377a1aa845940ead2e49640b74024fd01d
SOSTheBlack <jeancesargarcia@gmail.com>
2019-03-30 01:15:21 -0300

    esqueci o que eu fiz aqui

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,8 +4,7 @@ $factory->define(App\Entities\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'email_verified_at' => Carbon\Carbon::now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
     ];
 });
